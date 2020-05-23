22e599d8bf65a8609b5181fa9c26b5c9305773b3
yess-guns <yessy12384@gmail.com>
2020-05-22 23:12:00 -0500

    creación de la app core y sus urls

--- webempresa/urls.py
+++ webempresa/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
+    #Paths del core
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('services/', views.services, name="services"),
+    path('store/', views.store, name="store"),
+    path('contact/', views.contact, name="contact"),
+    path('blog/', views.blog, name="blog"),
+    path('sample/', views.sample, name="sample"),
+    #Paths del admin
     path('admin/', admin.site.urls),
 ]
