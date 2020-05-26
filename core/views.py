e01b0c5ed9123bf3de341f210adfea553467014b
yess-guns <yessy12384@gmail.com>
2020-05-25 23:23:45 -0500

    validación de formulario

--- core/views.py
+++ core/views.py
@@ -11,6 +11,5 @@ def about(request):
 def store(request):
     return render(request, "core/store.html")
 
-def contact(request):
-    return render(request, "core/contact.html")
+
 
--- webempresa/settings.py
+++ webempresa/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'contact',
     'pages.apps.PagesConfig',
     'blog',
      'ckeditor',
